afdb5d44311f709512f71a27468eaf9a26c1a678
Martin Bramwell <martinhbramwell@gmail.com>
2017-11-10 18:06:55 -0500

    Enhance (all): Adds permissions example
    Signed-off-by: Martin Bramwell <martinhbramwell@gmail.com>

--- packages/my-example-forum/lib/server/seed/seed_posts.js
+++ packages/my-example-forum/lib/server/seed/seed_posts.js
@@ -35,7 +35,7 @@ if (getSetting('forum.seedOnStart')) {
   };
 
   var createPost = function (slug, postedAt, username, thumbnail) {
-    
+
     const user = Users.findOne({username: username});
 
     var post = {
@@ -51,7 +51,7 @@ if (getSetting('forum.seedOnStart')) {
       post.thumbnailUrl = "/packages/example-forum/lib/assets/images/" + thumbnail;
 
     newMutation({
-      collection: Posts, 
+      collection: Posts,
       document: post,
       currentUser: user,
       validate: false
@@ -75,7 +75,7 @@ if (getSetting('forum.seedOnStart')) {
       comment.parentCommentId = parentComment._id;
 
     newMutation({
-      collection: Comments, 
+      collection: Comments,
       document: comment,
       currentUser: user,
       validate: false
@@ -89,7 +89,7 @@ if (getSetting('forum.seedOnStart')) {
       isDummy: true
     };
     newMutation({
-      collection: Users, 
+      collection: Users,
       document: user,
       validate: false
     });
@@ -145,6 +145,9 @@ if (getSetting('forum.seedOnStart')) {
     if (!Users.find().count()) {
       createDummyUsers();
     }
+    while (  Users.find().count() < 3 ) {
+      Meteor._sleepForMs(500);
+    };
     if (!Posts.find().count()) {
       createDummyPosts();
     }
